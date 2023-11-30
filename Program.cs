4847381def1dabc7e708155b9e64de08de2ce4aa
bhtyrbyr <bhtyr.byr@gmail.com>
2023-11-30 19:00:05 +0300

    A default host is configured. Created db in Inmemory and created a class to create data in it.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using fimple_bootcamp_week_1_homework.DBOperations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var _host = Host.CreateDefaultBuilder().ConfigureServices(
+    services =>
+    {
+        services.AddDbContext<LibraryDbContext>(options => options.UseInMemoryDatabase(databaseName: "LibraryManagerDB"));
+        services.AddScoped<ILibraryDbContext>(provider => provider.GetService<LibraryDbContext>());
+    }).Build();
+
+using (var scope = _host.Services.CreateAsyncScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+};
+
+var _libraryDbContext = _host.Services.GetRequiredService<ILibraryDbContext>();
+
+Console.WriteLine(_libraryDbContext.Books.FirstOrDefault(book => book.Id == 1).Title);
