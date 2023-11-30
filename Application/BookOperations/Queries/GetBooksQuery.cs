a97f89345389ec1b0196e38dbb6f859d30e90322
bhtyrbyr <bhtyr.byr@gmail.com>
2023-11-30 19:16:58 +0300

    A controller has been created for the books. A viewmodel was created to list the books.

---
+++ Application/BookOperations/Queries/GetBooksQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using fimple_bootcamp_week_1_homework.DBOperations;
+using fimple_bootcamp_week_1_homework.Entitys;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.Application.BookOperations.Queries
+{
+    public class BooksViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+    internal class GetBooksQuery
+    {
+        private readonly ILibraryDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public GetBooksQuery(ILibraryDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public List<BooksViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.Include(x => x.Author).OrderBy(x => x.Id).ToList();
+            return new List<BooksViewModel>(_mapper.Map<List<BooksViewModel>>(bookList)); 
+        }
+    }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using fimple_bootcamp_week_1_homework.Application.BookOperations.Queries;
+using fimple_bootcamp_week_1_homework.DBOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.Controllers
+{
+    internal class BookController
+    {
+        private readonly ILibraryDbContext _libraryDbContext;
+        private readonly IMapper _mapper;
+
+        public BookController(ILibraryDbContext libraryDbContext, IMapper mapper)
+        {
+            _libraryDbContext = libraryDbContext;
+            _mapper = mapper;
+        }
+
+        public List<BooksViewModel> ListBook()
+        {
+            GetBooksQuery query = new GetBooksQuery(_libraryDbContext, _mapper);
+            var result = query.Handle();
+            return result;
+        }
+    }
+}
