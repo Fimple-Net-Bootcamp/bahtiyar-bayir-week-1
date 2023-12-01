ef526a5c5f3e56f68feb7271df80f8a6a4d23234
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-01 16:51:40 +0300

    DTOs were created for authors.

---
+++ DTOs/AuthorDTO/CreateAuthorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.DTOs.AuthorDTO
+{
+    internal class CreateAuthorModel
+    {
+    }
+}
