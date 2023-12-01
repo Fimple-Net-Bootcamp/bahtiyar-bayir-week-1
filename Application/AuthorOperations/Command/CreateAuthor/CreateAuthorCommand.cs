b1e9e45d4c9e2c8491549f11c7df2d6433573e9c
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-01 17:02:39 +0300

    Added the ability to add authors.

---
+++ Application/AuthorOperations/Command/CreateAuthor/CreateAuthorCommand.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using fimple_bootcamp_week_1_homework.DBOperations;
+using fimple_bootcamp_week_1_homework.DTOs.AuthorDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.Application.AuthorOperations.Command.CreateAuthor
+{
+    internal class CreateAuthorCommand
+    {
+        public CreateAuthorModel Model { get; set; }
+        private readonly ILibraryDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public CreateAuthorCommand(ILibraryDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var member = _dbContext.Authors.SingleOrDefault(x => x.GetName().ToLower().Trim() == $"{Model.Name} {Model.Surname}".ToLower().Trim());
+            if (member is not null)
+                throw new InvalidOperationException($"Bu isimde bir üye kaydı daha önceden mevcut! Üye numarası {member.Id}");
+
+            member = _mapper.Map<Author>(Model);
+            _dbContext.Authors.Add(member);
+            _dbContext.SaveChanges();
+        }
+    }
+}
