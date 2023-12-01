88c18f2d464bf83747273f2fab4e104ead261c80
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-01 16:11:57 +0300

    Member creation feature has been added.

---
+++ Application/MemberOperations/Commands/CreateMember/CreateMemberCommand.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using fimple_bootcamp_week_1_homework.DBOperations;
+using fimple_bootcamp_week_1_homework.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.Application.MemberOperations.Commands.CreateMember
+{
+    public class CreateMemberModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDay { get; set; }
+
+    }
+    internal class CreateMemberCommand
+    {
+        public CreateMemberModel Model { get; set; }
+        private readonly ILibraryDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public CreateMemberCommand(ILibraryDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var member = _dbContext.Members.SingleOrDefault(x => x.GetName().ToLower().Trim() == $"{Model.Name} {Model.Surname}".ToLower().Trim());
+            if (member is not null)
+                throw new InvalidOperationException($"Bu isimde bir üye kaydı daha önceden mevcut! Üye numarası {member.Id}");
+
+            member = _mapper.Map<Member>(Model);
+            _dbContext.Members.Add(member);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Application/MemberOperations/Commands/CreateMember/CreateMemberCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fimple_bootcamp_week_1_homework.Application.MemberOperations.Commands.CreateMember
+{
+    internal class CreateMemberCommandValidator : AbstractValidator<CreateMemberCommand>
+    {
+        public CreateMemberCommandValidator()
+        {
+            RuleFor(command => command.Model.Name).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.Surname).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.BirthDay.Date).NotEmpty().LessThan(new DateTime(DateTime.Now.Year - 15, 1, 1));
+        }
+    }
+}
