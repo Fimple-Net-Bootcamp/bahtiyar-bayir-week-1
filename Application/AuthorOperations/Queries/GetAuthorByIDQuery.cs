519885db9188b745149a1f31d6f4f486230b8cc3
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-01 16:57:22 +0300

    Query by ID feature has been added for authors.

--- Application/AuthorOperations/Queries/GetAuthorByIDQuery.cs
+++ Application/AuthorOperations/Queries/GetAuthorByIDQuery.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using fimple_bootcamp_week_1_homework.DBOperations;
+using fimple_bootcamp_week_1_homework.DTOs.AuthorDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,20 @@ namespace fimple_bootcamp_week_1_homework.Application.AuthorOperations.Queries
 {
     internal class GetAuthorByIDQuery
     {
+        public int id;
+        private readonly ILibraryDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public GetAuthorByIDQuery(ILibraryDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public AuthorViewModel Handle()
+        {
+            var book = _dbContext.Authors.FirstOrDefault(b => b.Id == id);
+            if (book is null)
+                throw new ArgumentException($"{id} numarasına sahip bir kitap bulunamadı!");
+            return _mapper.Map<AuthorViewModel>(book);
+        }
     }
 }
